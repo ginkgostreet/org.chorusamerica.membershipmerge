92c573b49c3eb4bf415cc17e874099aa8c36e35c
Frank J. Gómez <frank@ginkgostreet.com>
2019-01-16 18:45:32 -0500

    CA2-367: Satisfied only-one-contact-at-a-time test.

--- api/v3/Membership/Merge.php
+++ api/v3/Membership/Merge.php
@@ -23,23 +23,18 @@ function _civicrm_api3_membership_Merge_spec(&$spec) {
  * @return array API result descriptor
  * @see civicrm_api3_create_success
  * @see civicrm_api3_create_error
- * @throws API_Exception
+ * @throws \CiviCRM_API_Exception
  */
 function civicrm_api3_membership_Merge($params) {
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array(
-      // OK, return several data rows
-      12 => array('id' => 12, 'name' => 'Twelve'),
-      34 => array('id' => 34, 'name' => 'Thirty four'),
-      56 => array('id' => 56, 'name' => 'Fifty six'),
-    );
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
+  try {
+    $raiseException = TRUE;
+    $contactId = CRM_Utils_Type::validate($params['contact_id'], 'Int', $raiseException, 'One of parameters ', $raiseException);
+  }
+  catch (\CRM_Core_Exception $e) {
+    throw new \CiviCRM_API3_Exception('Invalid format for contact_id', 'invalid_format', $params, $e);
+  }
 
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
-  }
-  else {
-    throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
-  }
+  $returnValues = array();
+
+  return civicrm_api3_create_success($returnValues, $params, 'Member', 'Merge');
 }
